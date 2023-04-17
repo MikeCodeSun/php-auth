bf25fc9a3f45ff20c1bb27c188493685d9ab978b
Mike、 <mike@MikedeMacBook-Pro.local>
2023-04-17 13:10:11 +0800

    php auth user login/register form &mysql

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+  include "auth.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+  <div class="text-center mt-5">
+    <h1>Hello <span class="text-info"><?php echo $_SESSION['name'] ?></span></h1>
+    <a role="button" class="btn btn-danger" href="logout.php">logout</a>
+  </div>
+</body>
+</html>
